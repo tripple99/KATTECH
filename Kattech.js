60f1579bb15276100ee9cb804ba6bfb8784db71a
tripple99 <tripple99cosmic@gmail.com>
2024-06-11 14:23:25 +0200

    KATTECH HUB

---
+++ Kattech.js
@@ -0,0 +1,13 @@
+let close = document.querySelector(".close");
+let classItems = document.querySelector(".items")
+let open = document.querySelector(".open")
+
+console.log(open);
+function openMenu(){
+   classItems.style.display = "block"
+}
+function closeMenu() {
+   classItems.style.display = "none"
+} 
+
+
